f05fa4d7536633055dc087370a55c1c9451b2358
vitorrobertogq <105520604+vitorrobertogq@users.noreply.github.com>
2022-12-26 15:37:44 -0300

    Mapa com posições

--- Batalha Naval/src/Mapa.java
+++ Batalha Naval/src/Mapa.java
@@ -1,19 +1,24 @@
-package java;
+package arquivos;
 import java.io.IOException;
 
 public class Mapa {
    
-    void map(int x,int y,int coluna) {
+    static void map() {
         String contorno = "+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n";
         String espaco = "|     |     |     |     |     |     |     |     |     |     |\n";
+        boolean[] navios = {false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
 
         char[] letras = {'A','B','C','D','E','F','G','H','I','J'};
 
-        for(int i = 0; i < coluna ;i++) {
+        for(int i = 0; i < 10 ;i++) {
             System.out.print("  " +contorno);
             System.out.print("  " + espaco);
-            System.out.print(letras[i] + " " + espaco);
-            System.out.print("  " + espaco);
+            System.out.print(letras[i] + " |");
+            for(int j =0;j < 10;j++){
+                if(navios[(i*10) +j ]){
+                System.out.print("  N  |");}
+                else {System.out.print("     |");}}
+            System.out.print("\n  " + espaco);
         }
         System.out.print("  " +contorno);
         System.out.print("     1     2     3     4     5     6     7     8     9     10");
@@ -22,5 +27,9 @@ public class Mapa {
     
     void clear() throws InterruptedException, IOException{
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+
+    public static void main(String[] args) {
+        map();
     }
 }

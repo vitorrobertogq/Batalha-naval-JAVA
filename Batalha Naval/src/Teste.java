9e64173af560666b9cd622029c6c48f37a954d2c
laf-z <19leo.oliveira@gmail.com>
2022-12-26 14:43:21 -0300

    teste

--- Batalha Naval/src/Teste.java
+++ Batalha Naval/src/Teste.java
@@ -2,6 +2,6 @@ public class Teste {
     public static void main(String[] args){
         char escCode = 0x1B;
         int row = 10; int column = 10;
-        System.out.print(String.format("%c[%d;%df",escCode,row,column));
+        System.out.print(String.format("%c[%d;%df",escCode,row,column)); //aaa 
     }
 }

89ab880bc0facbb3a41537bd54393ac35c9a1be7
vitorrobertogq <105520604+vitorrobertogq@users.noreply.github.com>
2022-12-26 08:23:00 -0300

    Update mapa.java
    clear function

--- mapa.java
+++ mapa.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
+
 class mapa {
-
     //APENAS O DESENHO DO MAPA
-
+   
     static void map(int x,int y,int coluna) {
         String contorno = "+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n";
         String espaco = "|     |     |     |     |     |     |     |     |     |     |\n";
@@ -16,9 +17,16 @@ class mapa {
         }
         System.out.print("  " +contorno);
         System.out.print("     1     2     3     4     5     6     7     8     9     10");
+
       }     
     
-    public static void main(String args[]) {
+    static void clear() throws InterruptedException, IOException{
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+
+    public static void main(String args[]) throws InterruptedException, IOException {
         map(0,0,10);
+        clear();
+        
     }
 }

99f1b5a631f1e15e5325b4ab260871f151813aa4
vitorrobertogq <105520604+vitorrobertogq@users.noreply.github.com>
2023-02-26 00:36:35 -0300

    Add a enemy map for atack

--- Batalha Naval/src/Mapa.java
+++ Batalha Naval/src/Mapa.java
@@ -1,31 +1,48 @@
 import java.io.IOException;
 
 public class Mapa {
-   
+    
     //printar o mapa (array de inteiros sendo 1 para a posiçao com navios e 0 para a sem navios)
-    void mapa1(int[] navios) {
-        String contorno = "+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n";
-        String espaco = "|     |     |     |     |     |     |     |     |     |     |\n";
+    static void mapa1(int[] meusNavios,int[] naviosInimigos) {
+        String contorno = "+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+";
+        String espaco = "|     |     |     |     |     |     |     |     |     |     |";
 
         char[] letras = {'A','B','C','D','E','F','G','H','I','J'};
 
+        System.out.println("\n\n\n"+"                                     Mapa de Defesa                                                              Mapa de Ataque"+ "\n\n\n");
         for(int i = 0; i < 10 ;i++) {
-            System.out.print("  " +contorno);
-            System.out.print("  " + espaco);
-            System.out.print(letras[i] + " |");
+            System.out.print("              " + contorno + "               " + contorno + "\n");
+            System.out.print("              " + espaco + "               " + espaco + "\n");
+            System.out.print("            " + letras[i] + " |");
             for(int j =0;j < 10;j++){
-                if(navios[(i*10) +j ] == 1){
+                if(meusNavios[(i*10) +j ] == 1){
                 System.out.print("  N  |");}
                 else {System.out.print("     |");}}
-            System.out.print("\n  " + espaco);
+            System.out.print("           ");
+            System.out.print("  " + letras[i] + " |");
+            for(int j =0;j < 10;j++){
+                if(naviosInimigos[(i*10) +j ] == 1){
+                System.out.print("  N  |");}
+                else {System.out.print("     |");}}
+            System.out.print("\n              " + espaco + "               " + espaco + "\n");
         }
-        System.out.print("  " +contorno);
-        System.out.print("     1     2     3     4     5     6     7     8     9     10");
+        System.out.print("              " + contorno + "               " + contorno + "\n");
+        System.out.print("                 1     2     3     4     5     6     7     8     9     10" + "                    " + "1     2     3     4     5     6     7     8     9     10");
 
       }     
     
     //funçao para limpar o console
     static void clear() throws InterruptedException, IOException{
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-    }}
-}
+    }
+
+    public static void main(String [] args) throws InterruptedException, IOException {
+        int[] tes = new int[100];
+        int[] tes1 = new int[100];
+        for(int i = 0;i < 100;i++){
+            tes[i] = 0;
+            tes1[i] = 0;}
+        clear();
+        mapa1(tes,tes1);
+      
+}}

2726b9b3bc8353c868ab649133964348f08c29aa
vitorrobertogq <mrvitonte@gmail.com>
2023-04-25 15:53:01 -0300

    rename

---
+++ console/src/jogo/principal.java
@@ -0,0 +1,14 @@
+package jogo;
+
+import java.io.IOException;
+
+import telas.menu;
+
+public class principal //RODE O CODIGO AQUI
+{
+    public static void main(String[] args) throws InterruptedException,IOException {
+    menu menu = new menu();
+    menu.printMenu();
+    }
+
+}
